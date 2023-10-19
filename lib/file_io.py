844c29b78da81896b5e5904c61ef2466e2175203
Kelvin okumba <okumbakelvin@gmail.com>
2023-10-19 14:28:15 +0300

    complet

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,16 @@
-def write_file(file_name, file_content):
-    pass
+# from file_io import write_file, append_to_file, read_file
 
-def append_file(file_name, append_content):
-    pass
+def write_file(file_name, file_content):
+     file_name_str = str(file_name)
+     with open(file_name_str + '.txt', 'w') as file:
+        file.write(file_content)
+
+def append_file(file_name, file_content):
+     file_name_str = str(file_name)
+     with open(file_name_str + '.txt', 'a') as file:
+        file.write(file_content)
 
 def read_file(file_name):
-    pass
+    file_name_str = str(file_name)
+    with open(file_name_str + '.txt', 'r') as file:
+        return file.read()
